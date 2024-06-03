22245dd7ae05efd2f28e61e7a46a8de21b856484
md-toybur-rahman <toyburrahman735@gmail.com>
2024-06-03 11:22:37 +0600

    some work remaining

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require("jsonwebtoken")
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,6 +9,38 @@ const port = process.env.PORT || 2000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+// JWT Token
+
+const CreateToken = (user) => {
+  const token = jwt.sign({
+    eamil: user.email
+  },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: '1h' }
+  );
+  return token;
+}
+
+// Verify Token
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {  
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded
+  });
+  next();
+}
+
+
+
 
 
 
@@ -29,15 +62,121 @@ async function run() {
 
     const itemCollection = client.db('fruit_brust').collection('all_fruit');
     const cartCollection = client.db('fruit_brust').collection('carts');
+    const usersCollection = client.db('fruit_brust').collection('users');
 
-	app.get('/items', async (req, res) => {
-		const cursor = itemCollection.find();
-		const result = await cursor.toArray();
-		res.send(result);
-	})
 
-  // hello
-    
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h'
+      })
+      res.send({ token, status: 'success' });
+    })
+
+    app.get('/items', async (req, res) => {
+      const id = req.query.id;
+      if (!id) {
+        const cursor = itemCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      }
+      else if (id) {
+        const query = { _id: new ObjectId(id) };
+        const result = await itemCollection.find(query).toArray();
+        res.send(result);
+      }
+      else {
+        res.send([])
+      }
+    })
+
+    app.post('/items', verifyToken, async (req, res) => {
+      const item = req.body;
+      const result = await itemCollection.insertOne(item);
+      res.send(result);
+    })
+    app.patch('/items', verifyToken, async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const item = req.body;
+      const updateDocument = {
+        $set: {
+          name: item.name,
+          price: item.price,
+          rating: item.rating,
+          category: item.category,
+          description: item.description,
+          imported_country: item.imported_country,
+          available_quantity: item.available_quantity,
+          image_url: item.image_url
+        }
+      }
+      const result = await itemCollection.updateOne(query, updateDocument)
+      res.send(result);
+    })
+    app.delete('/items', verifyToken, async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/users', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+      const query = { user_email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const userData = req.body;
+      const email = userData.email;
+      const query = { email: email }
+      const isUserExist = await usersCollection.findOne(query);
+      if (isUserExist) {
+        return res.send({
+          status: "success",
+          message: "Login Success",
+          email: email
+        });
+      }
+      else {
+        await usersCollection.insertOne(userData);
+        return res.send({
+          status: "success",
+          message: "Login Success",
+          email: email
+        })
+      }
+    })
+
+
+
+    app.post('/carts', verifyToken, async (req, res) => {
+      const cartData = req.body;
+      const result = await cartCollection.insertOne(cartData);
+      res.send(result)
+    })
+
+    app.delete('/carts', verifyToken, async (req, res) => {
+      const itemId = req.query.id;
+      const query = { _id: new ObjectId(itemId) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
@@ -56,9 +195,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-	res.send('Welcome to onePageProject Server')
+  res.send('Welcome to onePageProject Server')
 })
 
 app.listen(port, () => {
-	console.log(`Thei server is running on port ${port}`)
+  console.log(`Thei server is running on port ${port}`)
 })
